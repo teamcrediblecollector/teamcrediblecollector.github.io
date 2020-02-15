774b45219c62089fa9fa9bbe173c9f15154216bf
patbrown1357 <patbrown1357@gmail.com>
2020-02-15 15:24:29 -0500

    most recent site w/ node graph

--- api.py
+++ api.py
@@ -6,11 +6,14 @@ def main():
 
     with open( 'papers.json', 'w' ) as f:
         ads.config.token = ADS_DEV_KEY
-        papers = ads.SearchQuery( q ="supernova", sort='citation_count', fl=['title','citation_count'])
-        foundPapers = {};
+        papers = ads.SearchQuery( q ="rocket science", sort='citation_count', fl=['title','citation_count','year','author'])
+
+        foundPapers = []
+
         for paper in papers:
             print(paper.title)
-            foundPapers[str(paper.title)] = str(paper.citation_count)
+            foundPapers.append( { 'title':str(paper.title),'count':str(paper.citation_count), 'year':str( paper.year ) } )
+
 
     with open( 'papers.json', 'w' ) as f:
         json.dump( foundPapers, f )
